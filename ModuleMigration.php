fc41210f8895b87fd37034001384e3ca1bd3a3bc
Pavel Bariev <bariew@yandex.ru>
2014-05-27 19:18:52 +0400

    0.1.0: initial commit

---
+++ ModuleMigration.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * ModuleMigration class file
+ * @copyright Copyright (c) 2014 Galament
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace bariew\cmsBootstrap;
+
+use yii\console\controllers\MigrateController;
+
+/**
+ * Runs migrations from module /migrations folder.
+ * 
+ * @author Pavel Bariev <bariew@yandex.ru>
+ */
+class ModuleMigration extends MigrateController
+{
+    
+    public $allMigrationPaths = [];
+    public $migrationFiles = [];
+    
+    public function beforeAction($action)
+    {
+        if (!parent::beforeAction($action)) {
+            return false;
+        }
+        $this->allMigrationPaths['app'] = $this->migrationPath;
+        $this->attachModuleMigrations();
+        $this->setMigrationFiles();
+        return true;
+    }
+    
+    /**
+     * Returns the migrations that are not applied.
+     * @return array list of new migrations
+     */
+    protected function getNewMigrations()
+    {
+        $result = [];
+        foreach ($this->allMigrationPaths as $path) {
+            $this->migrationPath = $path;
+            $result = array_merge($result, parent::getNewMigrations());
+        }
+        $this->migrationPath = $this->allMigrationPaths['app'];
+        return $result;
+    }
+    
+    protected function getMigrationFile($name, $path = false)
+    {
+        $path = $path ? $path : $this->migrationPath;
+        return $path . DIRECTORY_SEPARATOR . $name . '.php';
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    protected function createMigration($class)
+    {
+        if (!$file = array_search($class, $this->migrationFiles)) {
+            return false;
+        }
+        require_once($file);
+
+        return new $class(['db' => $this->db]);
+    }
+    
+    protected function attachModuleMigrations()
+    {
+        $s = DIRECTORY_SEPARATOR;
+        foreach (\Yii::$app->modules as $name => $config) {
+            switch (gettype($config)) {
+                case 'object'   : 
+                    $basePath = $config->basePath;
+                    break;
+                case 'array'    : 
+                    if (isset($config['basePath'])) {
+                        $basePath = $config['basePath'];
+                        break;
+                    }
+                default         : 
+                    $basePath = \Yii::$app->basePath . "{$s}modules{$s}{$name}";
+            }
+            $this->allMigrationPaths[$name] = $basePath . $s . 'migrations';
+        }
+    }
+    
+    /**
+     * Returns the migrations that are not applied.
+     * @return array list of new migrations
+     */
+    protected function setMigrationFiles()
+    {
+        foreach ($this->allMigrationPaths as $path) {
+            $handle = opendir($path);
+            while (($file = readdir($handle)) !== false) {
+                if ($file === '.' || $file === '..') {
+                    continue;
+                }
+                $filePath = $path . DIRECTORY_SEPARATOR . $file;
+                if (preg_match('/^(m(\d{6}_\d{6})_.*?)\.php$/', $file, $matches) && is_file($filePath)) {
+                    $this->migrationFiles[$filePath] = $matches[1];
+                }
+            }
+            closedir($handle);
+        }
+        return $this->migrationFiles;
+    }
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+
+Module migration Yii2 controller.
+===================
+Runs module migrations from module 'migrations' folder.
+Use it if you want to keep your module migrations inside module 'migrations' folder.
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist bariew/yii2-module-migration-controller "*"
+```
+
+or add
+
+```
+"bariew/yii2-module-migration-controller": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+```
+    Redefine migrate controller in your console config file:
+    ...
+    'controllerMap' => [
+        'migrate'   => 'bariew\moduleMigration\ModuleMigration'
+    ]
+
+    also define 'modules' app attribute here like in web app config file.
+    ...
+    'modules'   => [
+        'user'  => 'app\modules\user\Module'
+    ]
+```
