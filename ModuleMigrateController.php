ca732a95e271ab4cd7ff3d12c4f88a6a1cd7fa1c
bariew home <bariew@yandex.ru>
2015-02-07 16:52:37 +0400

    fixed create before action schema flush

--- ModuleMigrateController.php
+++ ModuleMigrateController.php
@@ -39,9 +39,8 @@ class ModuleMigrateController extends MigrateController
         if (!parent::beforeAction($action)) {
             return false;
         }
-        $cache = $this->db->schemaCache;
-        if (is_object($cache)) {
-            $cache->flush();
+        if ($action->id !== 'create' && is_object($this->db->schemaCache)) {
+            $this->db->schemaCache->flush();
         }
         $this->allMigrationPaths['app'] = $this->migrationPath;
         $this->attachModuleMigrations();
